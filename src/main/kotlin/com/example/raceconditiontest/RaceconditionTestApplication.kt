3e4ff5d52a0aa553425a1c16cdcb640b311241d2
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-08-28 15:51:02 +0900

    init project

---
+++ src/main/kotlin/com/example/raceconditiontest/RaceconditionTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.raceconditiontest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RaceconditionTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<RaceconditionTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/raceconditiontest/RaceconditionTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.raceconditiontest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RaceconditionTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
