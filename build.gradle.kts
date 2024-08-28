4dcf4253d2e5c0fa752c7963e3dfbdc080599cfb
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-08-28 18:39:51 +0900

    update gradle, gitIgnore

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
+
+    // JPA
+    kotlin("plugin.jpa") version "1.4.32"
 }
 
 group = "com.example"
@@ -35,6 +38,15 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 kotlin {
